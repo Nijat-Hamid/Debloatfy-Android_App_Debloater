e615a5b064626d3d8150cf8f0ae600b6aaadd270
Nijat Hamid <nicatorium@gmail.com>
2025-02-12 16:24:53 +0400

    Initial Commit

---
+++ Debloatfy/DebloatfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebloatfyApp.swift
+//  Debloatfy
+//
+//  Created by Nijat Hamid on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DebloatfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
