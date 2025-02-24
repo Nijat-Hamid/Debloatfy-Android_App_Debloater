f85a48bcb435c3aa385d9b3a5bf8ff85f82c0473
Nijat Hamid <nicatorium@gmail.com>
2025-02-24 10:11:34 +0400

    Code Cleanup

--- Sources/Core/ViewModifiers/PageMod.swift
+++ Sources/Core/ViewModifiers/PageMod.swift
@@ -8,17 +8,9 @@
 import SwiftUI
 
 struct PageMod:ViewModifier {
-    
-    private let hideToolbar:Bool
-    
-    init(hideToolbar:Bool = false) {
-        self.hideToolbar = hideToolbar
-    }
-    
+
     func body(content: Content) -> some View {
         content
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .toolbar(hideToolbar ? .hidden : .automatic, for: .automatic)
-            .toolbarBackground(hideToolbar ? .hidden : .automatic, for: .automatic)
     }
 }
