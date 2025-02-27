166548580283e980a61b869db74f8903322cf445
Nijat Hamid <nicatorium@gmail.com>
2025-02-27 14:12:29 +0400

    Preview modifier updated to optional values

--- Sources/Core/ViewModifiers/PreviewMod.swift
+++ Sources/Core/ViewModifiers/PreviewMod.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct PreviewMod: ViewModifier {
     
-    private var width: CGFloat = 150
+    private var width: CGFloat?
     private var height: CGFloat?
-    private var padding: CGFloat = 10
+    private var padding: CGFloat?
     
-    init(width: CGFloat = 150, height: CGFloat? = nil, padding: CGFloat = 10) {
+    init(width: CGFloat? = 150, height: CGFloat? = nil, padding: CGFloat? = 10) {
         self.width = width
         self.height = height
         self.padding = padding
@@ -21,7 +21,7 @@ struct PreviewMod: ViewModifier {
     
     func body(content: Content) -> some View {
         content
-            .padding(padding)
+            .padding(.all, padding)
             .frame(width: width, height: height)
     }
 }
