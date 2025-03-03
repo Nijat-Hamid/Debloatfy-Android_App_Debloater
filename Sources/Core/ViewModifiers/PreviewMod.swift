555a04b297e0499da1f15ba32a4f1730e54e3590
Nijat Hamid <nicatorium@gmail.com>
2025-03-03 20:30:55 +0400

    Preview Modifier improved

--- Sources/Core/ViewModifiers/PreviewMod.swift
+++ Sources/Core/ViewModifiers/PreviewMod.swift
@@ -7,22 +7,40 @@
 
 import SwiftUI
 
+enum PreviewType {
+    case background,card,none
+}
+
 struct PreviewMod: ViewModifier {
     
-    private var width: CGFloat?
-    private var height: CGFloat?
-    private var padding: CGFloat?
+    private let width: CGFloat?
+    private let height: CGFloat?
+    private let padding: CGFloat?
+    private let previewType:PreviewType
     
-    init(width: CGFloat? = 150, height: CGFloat? = nil, padding: CGFloat? = 10) {
+    init(type:PreviewType = .background, width: CGFloat? = 150, height: CGFloat? = nil, padding: CGFloat? = 10) {
         self.width = width
         self.height = height
         self.padding = padding
+        self.previewType = type
+    }
+    
+    private var background:Color {
+        switch previewType {
+        case .background:
+            return Color.backgroundPrimary
+        case .card:
+            return Color.backgroundCard
+        case .none:
+            return Color.clear
+        }
     }
     
     func body(content: Content) -> some View {
         content
             .padding(.all, padding)
             .frame(width: width, height: height)
-            .background(Color.backgroundPrimary)
+            .background(background)
+        
     }
 }
