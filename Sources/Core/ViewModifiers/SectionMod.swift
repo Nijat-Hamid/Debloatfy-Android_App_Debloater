d147d0c71888e4899df7e836ce0dad1f5e14b47d
Nijat Hamid <nicatorium@gmail.com>
2025-03-18 15:19:13 +0400

    Section modifier updated: alignment property added

--- Sources/Core/ViewModifiers/SectionMod.swift
+++ Sources/Core/ViewModifiers/SectionMod.swift
@@ -15,14 +15,16 @@ enum SectionType {
 struct SectionMod:ViewModifier {
 
     private let sectionType:SectionType
+    private let alignment:Alignment
     
-    init(sectionType: SectionType = .fullWidth) {
+    init(sectionType: SectionType = .fullWidth,alignment:Alignment = .center) {
         self.sectionType = sectionType
+        self.alignment = alignment
     }
     
     func body(content: Content) -> some View {
         content
-            .frame(maxWidth: sectionType == .automatic ? nil : .infinity)
+            .frame(maxWidth: sectionType == .automatic ? nil : .infinity,alignment:alignment)
             .padding()
             .background(VisualEffect(.card))
             .clipShape(.rect(cornerRadius: 8))
