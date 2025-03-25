57aae8722e1c25fd0121dc170dbc03a513155eee
Nijat Hamid <nicatorium@gmail.com>
2025-03-25 21:48:53 +0400

    TransferView completed

--- Sources/Scenes/Transfer/View/TransferView.swift
+++ Sources/Scenes/Transfer/View/TransferView.swift
@@ -7,10 +7,33 @@
 
 import SwiftUI
 
+
 struct TransferView: View {
+    
+    @AppStorage("defaultLocation") private var defaultLocation:URL = Utils.getDownloadLocation()
+    
     var body: some View {
-        VStack {
-            Text("Transfer Page")
+        VStack(spacing:12) {
+            VStack(spacing:4) {
+                FolderListHeader()
+                ScrollView {
+                    LazyVStack {
+                        ForEach(0..<14) { item in
+                            FolderListItem(defaultLocation: defaultLocation,item:item)
+                        }
+                    }
+                }
+                .padding(.trailing, -16)
+            }
+            .modifier(SectionMod(sectionType: .fullWidth))
+            
+            HStack {
+                TransferActionButton(type: .destination(defaultLocation), defaultLocation: $defaultLocation)
+                Spacer()
+                TransferActionButton(defaultLocation: $defaultLocation)
+                
+            }
+            .modifier(SectionMod(sectionType: .fullWidth))
         }
     }
 }
