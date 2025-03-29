d92a0f0b12a28978befc47ea3f233fc5e2c24959
Nijat Hamid <nicatorium@gmail.com>
2025-03-29 11:10:52 +0400

    LoadingView added

---
+++ Sources/Core/Componets/Loading/LoadingView.swift
@@ -0,0 +1,61 @@
+//
+//  LoadingSpinner.swift
+//  Debloatfy
+//
+//  Created by Nijat Hamid on 3/27/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    @State private var loadingTexts = [
+        "Loading data...",
+        "Fetching resources...",
+        "Preparing content...",
+        "Almost there...",
+        "Just a moment..."
+    ]
+    
+    @State private var currentTextIndex = 0
+    
+    private let timer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
+    
+    private func startTextCycle() {
+        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
+            withAnimation {
+                currentTextIndex = (currentTextIndex + 1) % loadingTexts.count
+            }
+        }
+    }
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Image(systemName: "waveform")
+                .resizable()
+                .foregroundStyle(.secondary)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 60)
+                .symbolEffect(.bounce.byLayer, options: .repeat(.periodic(delay: 0.3)))
+            
+            Text(loadingTexts[currentTextIndex])
+                .foregroundStyle(.secondary)
+                .contentTransition(.numericText())
+                .font(.appHeadline)
+                .fontWeight(.medium)
+
+        }
+        .onReceive(timer) { _ in
+            withAnimation {
+                currentTextIndex = (currentTextIndex + 1) % loadingTexts.count
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.clear)
+    }
+}
+
+
+#Preview {
+    LoadingView()
+        .modifier(PreviewMod(type:.background,width: 300,padding: 24))
+}
