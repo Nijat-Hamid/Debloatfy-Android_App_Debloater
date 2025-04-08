a11bb1691f06bdcd7a1024d96d6430847759a94a
Nijat Hamid <nicatorium@gmail.com>
2025-04-08 22:21:18 +0400

    ContentUnaviable modifier improved

--- Sources/Core/Modifiers/UnavailableMod.swift
+++ Sources/Core/Modifiers/UnavailableMod.swift
@@ -12,31 +12,35 @@ enum UnavailableType {
     case empty
     case search
     case connection
+    case none
     
     var title:String {
         switch self {
-        case .error: return "Something went wrong"
-        case .empty: return "No data available"
-        case .search: return "No Results"
-        case .connection: return "USB connection error"
+        case .error: "Something went wrong"
+        case .empty: "No data available"
+        case .search: "No Results"
+        case .connection: "USB connection error"
+        case .none: ""
         }
     }
     
     var description:String {
         switch self {
-        case .error: return "Please try again later"
-        case .empty: return "No items to display"
-        case .search: return "Try a new search"
-        case .connection: return "Check USB Connection"
+        case .error: "Please try again later"
+        case .empty: "No items to display"
+        case .search: "Try a new search"
+        case .connection: "Check USB Connection"
+        case .none:  ""
         }
     }
     
     var image:String {
         switch self {
-        case .error: return "exclamationmark.triangle"
-        case .empty: return "viewfinder"
-        case .search: return "magnifyingglass"
-        case .connection: return "cable.connector.slash"
+        case .error: "exclamationmark.triangle"
+        case .empty: "viewfinder"
+        case .search: "magnifyingglass"
+        case .connection: "cable.connector.slash"
+        case .none:  ""
         }
     }
 }
@@ -52,23 +56,23 @@ struct UnavailableMod:ViewModifier {
     func body(content: Content) -> some View {
         content
             .overlay {
-                ContentUnavailableView {
-                    Image(systemName: type.image)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 32)
-                } description: {
-                    VStack(spacing:4) {
-                        Text("\(type.title)")
-                            .font(.appTitle)
-                        Text("\(type.description)")
-                            .font(.appHeadline)
+                if type != .none {
+                    ContentUnavailableView {
+                        Image(systemName: type.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 32)
+                    } description: {
+                        VStack(spacing:4) {
+                            Text("\(type.title)")
+                                .font(.appTitle)
+                            Text("\(type.description)")
+                                .font(.appHeadline)
+                        }
                     }
+                    .modifier(PageMod())
+                    .clipShape(.rect(cornerRadius: 8))
                 }
-                .modifier(PageMod())
-                .background(VisualEffect(.background))
-                .clipShape(.rect(cornerRadius: 8))
-                    
             }
     }
 }
