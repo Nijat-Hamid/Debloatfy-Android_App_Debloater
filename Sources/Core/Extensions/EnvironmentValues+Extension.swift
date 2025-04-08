89d10b40b3a9b1ac648e2aa3085254fd5a0289f3
Nijat Hamid <nicatorium@gmail.com>
2025-04-08 22:22:10 +0400

    EnviromentValues expanded with Auth and ViewModels

--- Sources/Core/Extensions/EnvironmentValues+Extension.swift
+++ Sources/Core/Extensions/EnvironmentValues+Extension.swift
@@ -9,4 +9,7 @@ extension EnvironmentValues {
     @Entry var layoutVM = LayoutVM()
     @Entry var router = Router()
     @Entry var auth = Auth.shared
+    @Entry var selectManager = SelectManager()
+    @Entry var debloatVM = DebloatVM()
+//    @Entry var 
 }
