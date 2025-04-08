99febfa05cd6c3756d25f1d938a3d417b29da9a2
Nijat Hamid <nicatorium@gmail.com>
2025-04-08 22:18:28 +0400

    ADB commands expanded. Argument logic changed

--- Sources/Core/Helpers/ADB.swift
+++ Sources/Core/Helpers/ADB.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct ADB {
     
-    static func run(arguments: [Commands],wait:Bool = false) async -> String? {
+    static func run(arguments: [Commands],wait:Bool = true) async -> String? {
         guard let path = Bundle.main.path(forResource: "adb", ofType: nil) else { return nil }
         
         return await withCheckedContinuation { continuation in
             let task = Process()
             task.launchPath = path
-            task.arguments = arguments.flatMap { $0.rawValue.split(separator: " ").map(String.init)}
+            task.arguments = arguments.flatMap { $0.raw.split(separator: " ").map(String.init)}
             let pipe = Pipe()
             task.standardOutput = pipe
             task.standardError = pipe
@@ -39,22 +39,31 @@ struct ADB {
 }
 
 extension ADB {
-    enum Commands:String {
-        case kill = "kill-server"
-        case start = "start-server"
-        case devices = "devices"
-        case reboot = "reboot"
-        case getBootloader = "shell getprop ro.bootloader"
-        case getCompany = "shell getprop ro.product.manufacturer"
-        case getModel = "shell getprop ro.product.model"
-        case getProduct = "shell getprop ro.product.name"
-        case getSecurityPatch = "shell getprop ro.build.version.security_patch"
-        case getAndroidVersion = "shell getprop ro.build.version.release"
-        case getBuildID = "shell getprop ro.build.id"
-        case getDeviceID = "shell getprop ro.serialno"
-        case getListSystemApps = "shell pm list packages -s"
-        case getListUserApps = "shell pm list packages -3"
-
+    enum Commands {
+        case kill, start, devices, reboot, getBootloader, getCompany, getModel, getProduct, getSecurityPatch, getAndroidVersion, getBuildID, getDeviceID, getListSystemApps, getListUserApps, getApksFullPath(String), getApkSize(String), uninstallApk(String), backupApk(String, String)
+        
+        var raw:String {
+            switch self {
+            case .kill: return "kill-server"
+            case .start: return "start-server"
+            case .devices: return "devices"
+            case .reboot: return "reboot"
+            case .getBootloader: return "shell getprop ro.bootloader"
+            case .getCompany: return "shell getprop ro.product.manufacturer"
+            case .getModel: return "shell getprop ro.product.model"
+            case .getProduct: return "shell getprop ro.product.name"
+            case .getSecurityPatch: return "shell getprop ro.build.version.security_patch"
+            case .getAndroidVersion: return "shell getprop ro.build.version.release"
+            case .getBuildID: return "shell getprop ro.build.id"
+            case .getDeviceID: return "shell getprop ro.serialno"
+            case .getListSystemApps: return "shell pm list packages -s"
+            case .getListUserApps: return "shell pm list packages -3"
+            case .getApksFullPath(let package): return "shell pm path \(package)"
+            case .getApkSize(let path): return "shell du -sm \(path)"
+            case .uninstallApk(let package): return "shell pm uninstall \(package)"
+            case .backupApk(let from, let to): return "pull \(from) \(to)"
+            }
+        }
 
     }
 }
