0ddb5fa9fb6e1dd9413ed2a63e816e5991f52fce
Nijat Hamid <nicatorium@gmail.com>
2025-04-09 23:20:31 +0400

    FileKit helpers added for File Management

--- Sources/Core/Helpers/FileKit.swift
+++ Sources/Core/Helpers/FileKit.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
-struct FilesManager {
-    static let core = FileManager.default
+struct FileKit {
+    static let manager = FileManager.default
+    
+    private static let appDir = manager.urls(for: .documentDirectory, in: .userDomainMask).first!
+    
+    static let backupDir = appDir.appending(path: "\(Utils.appName)").appending(component: "Backups")
+    
+    static let backupPath = backupDir.path()
+    
+    static let backupJson = backupDir.appending(component: "backup_info.json")
+    
+    static var existsBackupDir:Bool {
+        manager.fileExists(atPath: backupPath)
+    }
+    
+    static var existsBackupJson:Bool {
+        manager.fileExists(atPath: backupJson.path())
+    }
+    
+    static func createBackupDirIfNeeded() throws {
+        try manager.createDirectory(at: backupDir, withIntermediateDirectories: true)
+    }
+    
+    static func existsApkDir (_ packageName:String) ->Bool {
+        let packagePath = backupDir.appending(path: packageName).path()
+        
+        return manager.fileExists(atPath: packagePath)
+    }
+    
+    static func returnApkDir (_ packageName:String) -> URL {
+        return backupDir.appending(path: packageName)
+    }
 }
