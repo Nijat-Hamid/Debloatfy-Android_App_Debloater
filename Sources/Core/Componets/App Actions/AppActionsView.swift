77c6eb0345f311187142dcffdecd5d40b0ef5257
Nijat Hamid <nicatorium@gmail.com>
2025-04-09 23:23:19 +0400

    AppActionsView's functions adapted for RestoreView too.

--- Sources/Core/Componets/App Actions/AppActionsView.swift
+++ Sources/Core/Componets/App Actions/AppActionsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AppActionsView: View {
     @Environment(\.debloatVM) private var debloatVM
-  
+    @Environment(\.restoreVM) private var restoreVM
     
     private let width:CGFloat
     private let type:AppListType
@@ -28,15 +28,15 @@ struct AppActionsView: View {
     var body: some View {
         HStack(spacing:8) {
             AppActionItem(title: type.btnOne, icon: "arrow.trianglehead.2.clockwise",size: 12,type: .success) {
-                debloatVM.openSheet(type: .backup, package: item.package)
+                type == .debloat ? debloatVM.openSheet(type: .backup, package: item.package) : restoreVM.openSheet(type: .restore, package: item.package)
             }
             
             AppActionItem(title: type.btnTwo, icon: "arrow.up.trash",size: 12,type: .warning) {
-                debloatVM.openSheet(type: .backupRemove, package: item.package)
+                type == .debloat ? debloatVM.openSheet(type: .backupRemove, package: item.package) : restoreVM.openSheet(type: .restoreRemove, package: item.package)
             }
             
             AppActionItem(title: "Remove", icon: "trash",size: 12,type: .danger) {
-                debloatVM.openSheet(type: .remove, package: item.package)
+                type == .debloat ? debloatVM.openSheet(type: .remove, package: item.package) : restoreVM.openSheet(type: .remove, package: item.package)
             }
         }
         .frame(width:width)
