24cbc6c90e55e6f9dd31b76756db767a7a24d575
Nijat Hamid <nicatorium@gmail.com>
2025-04-14 13:16:03 +0400

    TransferModel added

--- Sources/Scenes/Transfer/Model/TransferModel.swift
+++ Sources/Scenes/Transfer/Model/TransferModel.swift
@@ -5,3 +5,33 @@
 //  Created by Nijat Hamid on 4/10/25.
 //
 
+import Foundation
+
+enum TransferModelType {
+    case folder, file
+}
+
+struct TransferModel: Identifiable, Equatable, Hashable {
+    var id: UUID = UUID()
+    var name:String = "N/A"
+    var size:String = "N/A"
+    var owner:String = "User"
+    var type:TransferModelType = .file
+}
+
+extension TransferModel:Mockable {
+    typealias MockType = TransferModel
+    
+    static var mock: TransferModel {
+        MockType(name: "My Pictures", size: "300",type: .folder)
+    }
+    
+    static var mockList: [TransferModel] {
+        [
+            MockType(name: "Videos", size: "1.4", type: .folder),
+            MockType(name: "Snap", size: "34", type: .folder),
+            MockType(name: "Mp3", size: "60", type: .folder),
+            MockType(name: "Hans Zimmer - Time", size: "5"),
+        ]
+    }
+}
