43f89802f3667b948f0693ef8a8f01f2bb8563ee
Nijat Hamid <nicatorium@gmail.com>
2025-05-04 13:30:58 +0400

    AppDelegate: When app terminated, adb server will cleanup

---
+++ Sources/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Debloatfy
+//
+//  Created by Nijat Hamid on 5/3/25.
+//
+import AppKit
+
+class AppDelegate: NSObject,NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+    func applicationWillTerminate(_ notification: Notification) {
+        Task {
+            await ADB.run(arguments: [.kill])
+//            print("adb killed")
+        }
+    }
+}
